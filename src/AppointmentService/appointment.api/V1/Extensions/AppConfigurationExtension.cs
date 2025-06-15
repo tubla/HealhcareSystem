846c119b154e2b88159065b2e9ea3f7d7ffc8bd5
RAHUL26R <rahulroy.profile@gmail.com>
2025-06-15 20:36:08 +0530

    fix

--- src/AppointmentService/appointment.api/V1/Extensions/AppConfigurationExtension.cs
+++ src/AppointmentService/appointment.api/V1/Extensions/AppConfigurationExtension.cs
@@ -6,12 +6,14 @@ namespace appointment.api.V1.Extensions
 {
     public static class AppConfigurationExtension
     {
-        public static void AddAzureAppConfigurationWithSecrets(this ConfigurationManager configuration)
+        public static void AddAzureAppConfigurationWithSecrets(this ConfigurationManager configuration, ILogger<AppConfigurationExtension> logger)
         {
+            logger.LogInformation($"Entering : AddAzureAppConfigurationWithSecrets");
             // Fetch connection string from Key Vault
+            logger.LogInformation($"KeyVault Url : {configuration["KeyVaultUrl"]!}");
             var secretClient = new SecretClient(new Uri(configuration["KeyVaultUrl"]!), new DefaultAzureCredential());
             var connectionString = secretClient.GetSecret("AppConfigConnection").Value.Value;
-
+            logger.LogInformation($"AppConfig Connection String : {connectionString}");
             if (string.IsNullOrEmpty(connectionString))
                 throw new ArgumentException("Failed to retrieve AppConfiguration connection string from Key Vault.");
 
@@ -22,6 +24,7 @@ namespace appointment.api.V1.Extensions
                        .ConfigureKeyVault(kv => kv.SetCredential(new DefaultAzureCredential()))
                        .UseFeatureFlags();
             });
+            logger.LogInformation($"Exiting : AddAzureAppConfigurationWithSecrets");
         }
     }
 }
