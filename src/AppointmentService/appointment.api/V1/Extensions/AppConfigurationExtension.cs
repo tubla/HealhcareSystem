11c07e3510e870ac4dda22c24cd920073ff79876
RAHUL26R <rahulroy.profile@gmail.com>
2025-06-16 16:37:24 +0530

    fix

--- src/AppointmentService/appointment.api/V1/Extensions/AppConfigurationExtension.cs
+++ src/AppointmentService/appointment.api/V1/Extensions/AppConfigurationExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
 namespace appointment.api.V1.Extensions;
@@ -7,64 +8,49 @@ public static class AppConfigurationExtension
 {
     public static void AddAzureAppConfigurationWithSecrets(this ConfigurationManager configuration, ILogger logger)
     {
-        var appConfigUrl = configuration.GetValue<string>("AppConfiguration:ConnectionString");
-        if (!string.IsNullOrWhiteSpace(appConfigUrl))
+        try
         {
+            logger?.LogInformation($"Entering : AddAzureAppConfigurationWithSecrets");
+
+            // Prioritize environment variable or configuration
+            var connectionString = configuration["AppConfiguration:ConnectionString"]
+                ?? Environment.GetEnvironmentVariable("AppConfiguration:ConnectionString");
+            logger?.LogInformation($"AppConfig Connection String : {connectionString}");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                var keyVaultUri = configuration["KeyVault:VaultUri"]
+                    ?? Environment.GetEnvironmentVariable("KeyVault:VaultUri")
+                    ?? "https://healthcare-vault.vault.azure.net/";
+                // Fetch connection string from Key Vault
+                logger?.LogInformation($"KeyVault Url : {keyVaultUri}");
+                var secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
+                connectionString = secretClient.GetSecret("AppConfigConnection").Value.Value;
+                logger?.LogInformation($"AppConfig Connection String from KeyVault: {connectionString}");
+            }
+            else
+            {
+                logger?.LogInformation("Using AppConfiguration connection string from environment variable.");
+            }
+
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentException("Failed to retrieve AppConfiguration connection string from Key Vault.");
+
             configuration.AddAzureAppConfiguration(options =>
             {
-                options.Connect(appConfigUrl)
+                options.Connect(connectionString)
                        .Select(KeyFilter.Any, LabelFilter.Null)
                        .ConfigureKeyVault(kv => kv.SetCredential(new DefaultAzureCredential()))
                        .UseFeatureFlags();
             });
+            logger?.LogInformation("Successfully connected to Azure AppConfiguration.");
+            logger?.LogInformation($"Exiting : AddAzureAppConfigurationWithSecrets");
+        }
+        catch (Exception ex)
+        {
+            logger?.LogError(ex, "Failed to initialize Azure AppConfiguration with KeyVault URI: {KeyVaultUri}", configuration["KeyVault:VaultUri"]);
+            throw;
         }
 
-        //public static void AddAzureAppConfigurationWithSecrets(this ConfigurationManager configuration, ILogger logger)
-        //{
-        //    try
-        //    {
-        //        logger?.LogInformation($"Entering : AddAzureAppConfigurationWithSecrets");
 
-        //        // Prioritize environment variable or configuration
-        //        var connectionString = configuration["AppConfiguration:ConnectionString"]
-        //            ?? Environment.GetEnvironmentVariable("AppConfiguration:ConnectionString");
-        //        logger?.LogInformation($"AppConfig Connection String : {connectionString}");
-        //        if (string.IsNullOrEmpty(connectionString))
-        //        {
-        //            var keyVaultUri = configuration["KeyVault:VaultUri"]
-        //                ?? Environment.GetEnvironmentVariable("KeyVault:VaultUri")
-        //                ?? "https://healthcare-vault.vault.azure.net/";
-        //            // Fetch connection string from Key Vault
-        //            logger?.LogInformation($"KeyVault Url : {keyVaultUri}");
-        //            var secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
-        //            connectionString = secretClient.GetSecret("AppConfigConnection").Value.Value;
-        //            logger?.LogInformation($"AppConfig Connection String from KeyVault: {connectionString}");
-        //        }
-        //        else
-        //        {
-        //            logger?.LogInformation("Using AppConfiguration connection string from environment variable.");
-        //        }
-
-        //        if (string.IsNullOrEmpty(connectionString))
-        //            throw new ArgumentException("Failed to retrieve AppConfiguration connection string from Key Vault.");
-
-        //        configuration.AddAzureAppConfiguration(options =>
-        //        {
-        //            options.Connect(connectionString)
-        //                   .Select(KeyFilter.Any, LabelFilter.Null)
-        //                   .ConfigureKeyVault(kv => kv.SetCredential(new DefaultAzureCredential()))
-        //                   .UseFeatureFlags();
-        //        });
-        //        logger?.LogInformation("Successfully connected to Azure AppConfiguration.");
-        //        logger?.LogInformation($"Exiting : AddAzureAppConfigurationWithSecrets");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        logger?.LogError(ex, "Failed to initialize Azure AppConfiguration with KeyVault URI: {KeyVaultUri}", configuration["KeyVault:VaultUri"]);
-        //        throw;
-        //    }
-
-
-        //}
     }
 }
