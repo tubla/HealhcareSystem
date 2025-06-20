358a2fe3297cbe42be80d6d89b1d2b8a2c6ba205
RAHUL26R <rahulroy.profile@gmail.com>
2025-06-20 20:10:14 +0530

    corrected table schema

--- src/FlywayMigrations/V03__CreateHealthcareSchemas.sql
+++ src/FlywayMigrations/V03__CreateHealthcareSchemas.sql
@@ -1,7 +1,7 @@
 
 -- media
 create table [healthcare].[media] (
-    media_id INT identity(1,1) primary key,
+    media_id int identity(1,1) primary key,
     file_name nvarchar(100) not null,
     file_url nvarchar(500) not null,
     upload_date date not null default getdate(),
@@ -12,7 +12,7 @@ go
 -- prescription_media
 create table [healthcare].[prescription_media] (
 	prescription_id int not null
-    media_id INT int not null,
+    media_id int not null,
     primary key(prescription_id, media_id),
     foreign key(prescription_id) references [healthcare].[prescription](prescription_id) on delete cascade,
     foreign key(media_id) references [healthcare].[media](media_id) on delete cascade);
